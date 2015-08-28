f3fea156b7b8d50727765e6911094c9008549863
brianpaff1992 <brianpaff1992@gmail.com>
2015-08-28 15:26:46 -0700

    Added quick comments
    To RectangleAverager in exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -6,12 +6,14 @@ public class RectangleAverager {
         int totalArea = 0;
         float average;
 
+        //Get the total area first
         for (Rectangle r: rectangles)
         {
             int areaOfRectangle = r.area();
             totalArea = totalArea + areaOfRectangle;
         }
 
+        //Get the average by dividing the total area by the amount of rectangles
         average = (float) totalArea / rectangles.length;
         return average;
     }
