13e95e2ce26a4cbc4b16c8ab5cf88e8e3d35c969
brianpaff1992 <brianpaff1992@gmail.com>
2015-08-31 16:53:23 -0700

    Exercise9 done

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 // Exercise #9: Create a class Node that implements a binary tree for Strings. It should have an add(String name) method
 // that recursively finds the correct place to add a new node and a names() method that returns the names of all of the
@@ -15,56 +16,68 @@ public class Node {
 
     public void add(String nameOfNewNode) {
         //check if nameOfNewNode is smaller alphabetically
-        if(name.compareTo(nameOfNewNode) < 0)
+        if(name.compareTo(nameOfNewNode) > 0)
         {
-            //if nameOfNewNode belongs on the left side of our tree, make sure the current node is not a leaf node.
-            //if it is, create a new node
-            //if not, call add() on the left node
-            if(left == null)
-            {
-                left = new Node(nameOfNewNode);
-            }
-            else {
-                left.add(nameOfNewNode);
-            }
+           addLeft(nameOfNewNode);
         }
 
         //if nameOfNewNode belongs on the right side of our tree, make sure the current node is not a leaf node.
         //if it is, create a new node
         //if not, call add() on the right node
-        else if (name.compareTo(nameOfNewNode) > 0)
+        else if (name.compareTo(nameOfNewNode) < 0)
         {
-            if(right == null)
-            {
-                right = new Node(nameOfNewNode);
-            }
-            else {
-                right.add(nameOfNewNode);
-            }
+          addRight(nameOfNewNode);
         }
         else
         {
-            //do nothing, exercise did not mention whether to keep all entires, or remove duplicates
+            //do nothing, exercise did not mention whether to keep all entries, or remove duplicates
         }
 
+    }
+
+    private void addLeft(String nameOfNewNode)
+    {
+        //if nameOfNewNode belongs on the left side of our tree, make sure the current node is not a leaf node.
+        //if it is, create a new node
+        //if not, call add() on the left node
+        if(left == null)
+        {
+            //is a leaf node
+            left = new Node(nameOfNewNode);
+        }
+        else {
+            left.add(nameOfNewNode);
+        }
+    }
+
+    private void addRight(String nameOfNewNode)
+    {
+        if(right == null)
+        {
+            //is a leaf node
+            right = new Node(nameOfNewNode);
+        }
+        else {
+            right.add(nameOfNewNode);
+        }
     }
 
     public List<String> names() {
+        List<String> returnList = new ArrayList<String>();
 
-        if(left == null)
+        if(left != null)
         {
-            return name;
+            returnList.addAll(left.names());
         }
 
-        List<String> returnList = left.names();
-        List<String> rightList = right.names();
+        returnList.add(name);
 
-        returnList.addAll(rightList);
+        if(right != null)
+        {
+            returnList.addAll(right.names());
+        }
 
-    }
+        return returnList;
 
-    public String name()
-    {
-        return name;
     }
 }
